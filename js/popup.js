8657e6aac24250e102f68880fe524aedc0f30f16
kimnari88 <99231056+kimnari88@users.noreply.github.com>
2022-03-24 13:23:50 +0900

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,102 @@
+$(function () {
+  $(".pu1").click(function () {
+    $(".popup1").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+  $(".close").click(function () {
+    $(".popup").fadeOut();
+  });//.pupup p a를 클릭했을때 이벤트
+
+  $(".pu2").click(function () {
+    $(".popup2").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu3").click(function () {
+    $(".popup3").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu4").click(function () {
+    $(".popup4").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu5").click(function () {
+    $(".popup5").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu6").click(function () {
+    $(".popup6").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu7").click(function () {
+    $(".popup7").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu8").click(function () {
+    $(".popup8").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu9").click(function () {
+    $(".popup9").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu10").click(function () {
+    $(".popup10").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu11").click(function () {
+    $(".popup11").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu12").click(function () {
+    $(".popup12").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu13").click(function () {
+    $(".popup13").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu14").click(function () {
+    $(".popup14").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu15").click(function () {
+    $(".popup15").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu16").click(function () {
+    $(".popup16").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu17").click(function () {
+    $(".popup17").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu18").click(function () {
+    $(".popup18").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu19").click(function () {
+    $(".popup19").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu20").click(function () {
+    $(".popup20").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu21").click(function () {
+    $(".popup21").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu22").click(function () {
+    $(".popup22").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu23").click(function () {
+    $(".popup23").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+  $(".pu24").click(function () {
+    $(".popup24").fadeIn();
+  });//#container a를 클릭했을때 이벤트
+
+
+});//기본구조
